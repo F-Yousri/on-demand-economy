e58284c5d9e6ff334d4feb2c4b3c280e06d483ba
Minos32x <minasamy65@gmail.com>
2018-06-13 14:05:45 +0200

    change image qualtiy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,7 +13,7 @@ class OrdersController < ApplicationController
     end
     def show
         pending_orders=Order.where(created_by: current_user.id,status: "pending").order(time: :desc)
-        active_orders=Order.where(created_by: current_user.id,status: "active").order(time: :desc).page(params[:page_number])
+        active_orders=Order.where(created_by: current_user.id,status: "active").order(time: :desc)
         upcomig_orders=Order.where(created_by: current_user.id,status: "upcoming").order(time: :desc).page(params[:page_number])
         history_orders=Order.where(created_by: current_user.id,status: "history").order(time: :desc).page(params[:page_number])
         history_pages=history_orders.total_pages
