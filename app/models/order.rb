e6f7745e96140748683d211a7def8896970f72bb
Minos32x <minasamy65@gmail.com>
2018-06-19 18:25:48 +0200

    remove image validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
     paginates_per 3
-    validates_presence_of :src_latitude,:src_longitude,:dest_latitude,:dest_longitude,:title, :provider_id,:payment_method,:weight,:images
+    validates_presence_of :src_latitude,:src_longitude,:dest_latitude,:dest_longitude,:title, :provider_id,:payment_method,:weight
     mount_uploader :images, GlobalUploader
 
     def order_id
