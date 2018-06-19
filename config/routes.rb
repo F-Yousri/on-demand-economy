c9400a56946e2a9373f72e6f0ce533434d5f27f5
minaibrahimsaid <minaibrahim1991@yahoo.com>
2018-06-19 17:20:14 +0200

    edit cancel url

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get '/api/v1/payments', to: "payments#show"
   post '/api/v1/payments' , to: 'payments#create'
   get '/api/v1/about', to: "users#about_us"
-  patch 'api/v1/orders/:order_id', to: 'orders#cancel'
+  patch 'api/v1/orders/cancel/:order_id', to: 'orders#cancel'
 
   # https://driveo.herokuapp.com/api/v1/authentication/resetpassword?hash=#{@reset_token}
 end
