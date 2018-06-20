f386fbb7c1353d53a733b416d375245cecc62f94
Minos32x <minasamy65@gmail.com>
2018-06-20 09:06:41 +0200

    disable sms in resend_verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,12 +56,12 @@ class UsersController < ApplicationController
     verificationCode = rand(1000..9999)
     user.user_pin = verificationCode
     if user.save
-      client = Twilio::REST::Client.new(Rails.application.secrets.sms_sid, Rails.application.secrets.sms_token)
-        client.api.account.messages.create(
-          from: Rails.application.secrets.sms_sender,
-          to: '+2'+user.phone,
-          body: "Thanks #{user.name} for signing up. Your New Verification Code is #{verificationCode} . \n "
-          )
+      # client = Twilio::REST::Client.new(Rails.application.secrets.sms_sid, Rails.application.secrets.sms_token)
+      #   client.api.account.messages.create(
+      #     from: Rails.application.secrets.sms_sender,
+      #     to: '+2'+user.phone,
+      #     body: "Thanks #{user.name} for signing up. Your New Verification Code is #{verificationCode} . \n "
+      #     )
       response={message: Message.success,user:user}
     end
   else
