b4da3ec76c2f7b0b98265530c3d27ca2f6e005af
Minos32x <minasamy65@gmail.com>
2018-06-20 02:09:19 +0200

    validates on numeric values

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
     paginates_per 3
     validates_presence_of :src_latitude,:src_longitude,:dest_latitude,:dest_longitude,:title, :provider_id,:payment_method,:weight
+    validates :provider_id , :numericality => { :greater_than_or_equal_to => 1 }
+    validates :weight , :numericality => { :greater_than_or_equal_to => 0.1 }
     mount_uploader :images, GlobalUploader
 
     def order_id
